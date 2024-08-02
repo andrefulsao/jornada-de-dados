557318bc9a9fe478fa0dd82d31b48d0992b77c91
André Mendes Nunes <branco.de@hotmail.com>
2024-08-02 00:12:01 -0300

    salvando projeto

---
+++ aula-08/etl.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+import glob
+
+def extrair_dados_e_consolidar(pasta: str) -> pd.DataFrame:
+    """
+    Uma função de extract que le e consolida os jsons
+    """
+    arquivos_json = glob.glob(os.path.join(pasta, '*.json'))
+    df_list = [pd.read_json(arquivo) for arquivo in arquivos_json]
+    df_total = pd.concat(df_list, ignore_index=True)
+    return df_total
+
+def calcular_kpi_de_total_de_vendas(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Uma função que transforma
+    """
+    df["Total"] = df["Quantidade"] * df["Venda"]
+
+    return df
+
+def carregar_dados(formato_saida: list, df: pd.DataFrame):
+    """
+    Uma função que da load em csv ou parquet
+    """
+    if "csv" in formato_saida:
+        df.to_csv("dados.csv", index=False)
+    if "parquet" in formato_saida:
+        df.to_parquet("dados.parquet", index=False)
+
+def pipeline_calcular_kpi_de_vendas_consolidado(pasta: str, formato_saida: list):    
+    data_frame = extrair_dados_e_consolidar(pasta=pasta)
+    date_frame_calculado = calcular_kpi_de_total_de_vendas(data_frame)    
+    carregar_dados(formato_saida, date_frame_calculado)
